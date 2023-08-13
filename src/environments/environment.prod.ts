c5cb04b5f6a25c7b22e52e2f2b12b269ba45b99a
petru.mada <petru.mada@fortech.ro>
2023-08-13 11:12:20 +0300

    APP-5: change to default app start on docker with production configuration to test render until i will make a better aproach

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import 'zone.js/plugins/zone-error'; // Included with Angular CLI.
 
 export const environment = {
-  url: 'https://localhost:8080',
-  production: true
+  url: 'https://gorrila-be.onrender.com',
+  production: false
 };
